9afa1f48250418986642d56d5488371f51000b58
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2023-04-02 19:54:56 +0800

    first commit

---
+++ link.py
@@ -0,0 +1,23 @@
+# import cx_Oracle
+# connection = cx_Oracle.connect('GROUP4', 'CkVpA3IHJR', cx_Oracle.makedsn(
+#     '140.117.69.70', 1521, service_name='ORCLPDB1'))
+# cursor = connection.cursor()
+import getpass
+import oracledb
+
+connection = oracledb.connect(
+    user="GROUP4",
+    password='CkVpA3IHJR',
+    dsn="140.117.69.70/ORCLPDB1")
+
+print("Successfully connected to Oracle Database")
+cursor = connection.cursor()
+
+# Create a table
+
+# x = cursor.execute(
+#     """
+#     select * from table1
+# """)
+
+# print(x.rowcoumt)
